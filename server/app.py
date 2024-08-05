c9cd229cafa510dc345855b6bb194adedf7bf9da
brewchetta <chett.tiller@gmail.com>
2024-08-05 13:16:31 -0400

    candies vs vegetables who will win?

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 
 from flask import request
 from config import app, db
-# from models import MODELS GO HERE
+from models import Candy
 
 
 # ROUTES
@@ -10,6 +10,37 @@ from config import app, db
 @app.get('/')
 def index():
     return { "response": "hello world" }, 200
+
+# READ ALL
+
+@app.get('/candies')
+def all_candies():
+    # all_the_candies = Candy.query.all()
+    # candy_dictionaries = []
+    # for candy in all_the_candies:
+    #     candy_dict = candy.to_dict()
+    #     candy_dictionaries.append( candy_dict )
+    # return candy_dictionaries, 200
+    return [ candy.to_dict() for candy in  Candy.query.all() ], 200
+
+# READ BY ID
+
+@app.get('/candies/<int:id>')
+def candy_by_id(id):
+    found_candy = Candy.query.where(Candy.id == id).first()
+
+    if found_candy:
+        return found_candy.to_dict(), 200
+    else:
+        return { 'error': 'Candy not found' }, 404
+
+# CREATE
+
+# UPDATE
+
+# DESTROY
+
+
 
 
 # APP RUN
--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from config import app, db
-# from models import MODELS GO HERE
+from models import Candy
 from faker import Faker
+import random
 
 faker = Faker()
 
@@ -10,6 +11,31 @@ if __name__ == '__main__':
     with app.app_context():
         print("Seeding database...")
 
-        # write your seeds here!
+        Candy.query.delete()
+
+        # MANUAL WAY
+
+        c1 = Candy(name="Reese's", brand="Hersheys", price=2)
+        c2 = Candy(name="Skittles", brand="Mars", price=3)
+        c3 = Candy(name="Snickers", brand="Mars", price=3)
+
+        db.session.add_all( [c1, c2, c3] )
+        db.session.commit()
+
+        print("CANDIES ADDED THE MANUAL WAY...")
+
+        # END MANUAL WAY
+
+        # THE LOOP WAY
+
+        for _ in range(1000):
+            new_candy = Candy( name=faker.name(), brand=faker.address(), price=random.choice( range( 1, 7 ) ) )
+            db.session.add( new_candy )
+
+        db.session.commit()
+
+        print("CANDIES ADDED THE LOOPING WAY...")
+
+        # END LOOP WAY
 
         print("Seeding complete!")
